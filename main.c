1b9b9f21a381160118fc11c78c7ce269692f673e
Jonas G <54191355+TerritorialBreak5@users.noreply.github.com>
2022-01-20 12:26:00 +0000

    Update main.c

--- main.c
+++ main.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <locale.h>
 
-#include "functions.c" // Importa as funções para o código
+#include "functions.c"  // Importa as funÃ§Ãµes para o cÃ³digo 
 
-// Função principal (primeira a ser chamada quando o programa inicia)
+// FunÃ§Ã£o principal (primeira a ser chamada quando o programa inicia)
 int main() {
 	int respostaAumentarRegistos;
-	// Coloca o idioma do programa em português
+	// Coloca o idioma do programa em portuguÃªs
 	setlocale(LC_ALL, "Portuguese");
 
-	// Chama a função para aumentar o maximo de registos que pode introduzir
+	// Chama a funÃ§Ã£o para aumentar o maximo de registos que pode introduzir
 	respostaAumentarRegistos = aumentarMaxRegistos();
-	// Verifica se não ocorreu nenhum erro a aumentar o maximo de registos
+	// Verifica se nÃ£o ocorreu nenhum erro a aumentar o maximo de registos
 	if (respostaAumentarRegistos == 1) {
-		// Chama a função para mostrar o menu principal
+		// Chama a funÃ§Ã£o para mostrar o menu principal
 		menuPrincipal();
 	}
 	else {
@@ -22,4 +22,3 @@ int main() {
 	}
 	return 0;
 }
-
